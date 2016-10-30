a3f350e2afe5eab090eb0d4f03b44a275f3e3792
CMRaper <raper_c@ymail.com>
2016-10-30 09:55:51 +0800

    name.php added for require_once

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-
- echo "Hello World!";
- echo "<br/> How are you today?";
- echo "<br/> Is it a good day?";
- echo "<br/>" . date("Y-m-d");
+	require_once("name.php");
+	 echo "Hello World!";
+	 echo "<br/> How are you today ".  $name ."?";
+	 echo "<br/> Is it a good day?";
+	 echo "<br/>" . date("Y-m-d");
 
 ?>
